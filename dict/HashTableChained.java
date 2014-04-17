2bfe6535f1588b293e3e957c8fc1ea197b45dd60
James Carr <jcarr@berkeley.edu>
2014-04-16 19:24:12 -0700

    cleaned up HashTable

--- dict/HashTableChained.java
+++ dict/HashTableChained.java
@@ -9,14 +9,6 @@ import list.*;
 
 /**
  *  HashTableChained implements a Dictionary as a hash table with chaining.
- *  All objects used as keys must have a valid hashCode() method, which is
- *  used to determine which bucket of the hash table an entry is stored in.
- *  Each object's hashCode() is presumed to return an int between
- *  Integer.MIN_VALUE and Integer.MAX_VALUE.  The HashTableChained class
- *  implements only the compression function, which maps the hash code to
- *  a bucket in the table's range.
- *
- *  DO NOT CHANGE ANY PROTOTYPES IN THIS FILE.
  **/
 
 public class HashTableChained implements Dictionary {
@@ -33,58 +25,48 @@ public class HashTableChained implements Dictionary {
 
   /** 
    *  Construct a new empty hash table intended to hold roughly sizeEstimate
-   *  entries.  (The precise number of buckets is up to you, but we recommend
-   *  you use a prime number, and shoot for a load factor between 0.5 and 1.)
+   *  entries.  
    **/
 
   public HashTableChained(int sizeEstimate) {
-    // Your solution here.
       if (bigN <= 0) {
-          //System.out.println("here");
           bigN += sizeEstimate;
       }
       while (!primeNumber(bigN)) {
-          bigN++; //increment by one until prime
+          bigN++; 
       }
-      table = new DList[bigN]; //bunch of DList2s
+      table = new DList[bigN]; 
       for (int i = 0; i < table.length; i++) {
-          //System.out.println( "hello " + i);
           DList insertion = new DList();
           this.table[i] = insertion;
       }
   }
 
   /** 
-   *  Construct a new empty hash table with a default size.  Say, a prime in
-   *  the neighborhood of 100.
+   *  Construct a new empty hash table with a default size.
    **/
 
   public HashTableChained() {
-    // Your solution here.
      this(103);
   }
 
   /**
    *  Converts a hash code in the range Integer.MIN_VALUE...Integer.MAX_VALUE
    *  to a value in the range 0...(size of hash table) - 1.
-   *
-   *  This function should have package protection (so we can test it), and
-   *  should be used by insert, find, and remove.
    **/
   
   public boolean primeNumber(int j) {
-      for (int i = 2; i < (j/2); i++) { //halfway trick
-          if (j%i == 0) { //exactly divisible, not prime
+      for (int i = 2; i < (j/2); i++) {
+          if (j%i == 0) {
               return false;
           }
       }
       return true;
   }
 
-  int compFunction(int code) {
-    // Replace the following line with your solution.
+  public int compFunction(int code) {
     long toReturn;
-      toReturn = ((127 * code + 65993) % 1999993) % table.length; //double modulus method
+      toReturn = ((127 * code + 65993) % 1999993) % table.length;
         if (toReturn < 0)
             toReturn += table.length;
         return (int) toReturn;  
@@ -98,7 +80,6 @@ public class HashTableChained implements Dictionary {
    **/
 
   public int size() {
-    // Replace the following line with your solution.
     return size;
   }
 
@@ -109,66 +90,47 @@ public class HashTableChained implements Dictionary {
    **/
 
   public boolean isEmpty() {
-    // Replace the following line with your solution.
     return size==0;
   }
 
   /**
-   *  Create a new Entry object referencing the input key and associated value,
-   *  and insert the entry into the dictionary.  Return a reference to the new
-   *  entry.  Multiple entries with the same key (or even the same key and
-   *  value) can coexist in the dictionary.
-   *
-   *  This method should run in O(1) time if the number of collisions is small.
-   *
    *  @param key the key by which the entry can be retrieved.
    *  @param value an arbitrary object.
    *  @return an entry containing the key and value.
    **/
 
   public Entry insert(Object key, Object value) {
-    // Replace the following line with your solution.
     Entry toInsert = new Entry();
     toInsert.key = key;
     toInsert.value = value;
     int hash = compFunction(key.hashCode());
-    //System.out.println("hash " + hash);
     this.table[hash].insertFront(toInsert);
     size++;
-    if(size/bigN > .75) { //make a new one! takes forever lol
+    if(size/bigN > .75) {
         HashTableChained newOne = new HashTableChained(2*this.size);
         for (DList dlist : table) {
             DListNode current = (DListNode)dlist.front();
-            int i = 0;
             while (current.isValidNode()) {
                 try {
-                    //System.out.println("count " + i);
                     newOne.insert(((Entry)(current.item())).key, ((Entry)(current.item())).value);
                     current = (DListNode)current.next();
-                    i++;
                 } catch (InvalidNodeException ex) {
-                    //System.out.println("TOO FAR");
+                    System.out.println("TOO FAR");
                 }
             }
         }
     }
-    return toInsert; //why...
+    return toInsert;
   }
 
   /** 
-   *  Search for an entry with the specified key.  If such an entry is found,
-   *  return it; otherwise return null.  If several entries have the specified
-   *  key, choose one arbitrarily and return it.
-   *
-   *  This method should run in O(1) time if the number of collisions is small.
-   *
+   *  Search for an entry with the specified key. 
    *  @param key the search key.
    *  @return an entry containing the key and an associated value, or null if
    *          no entry contains the specified key.
    **/
 
   public Entry find(Object key) {
-    // Replace the following line with your solution.
     int correctIndex = compFunction(key.hashCode());
     DList correctDList = this.table[correctIndex];
     if (correctDList == null) {
@@ -183,7 +145,6 @@ public class HashTableChained implements Dictionary {
             return (Entry)current.item();
         }
         catch (InvalidNodeException e1) {
-            //System.out.println("NOT FOUND");
             return null;
         }
     }
@@ -193,21 +154,13 @@ public class HashTableChained implements Dictionary {
   }
 
   /** 
-   *  Remove an entry with the specified key.  If such an entry is found,
-   *  remove it from the table and return it; otherwise return null.
-   *  If several entries have the specified key, choose one arbitrarily, then
-   *  remove and return it.
-   *
-   *  This method should run in O(1) time if the number of collisions is small.
-   *
+   *  Remove an entry with the specified key.
    *  @param key the search key.
    *  @return an entry containing the key and an associated value, or null if
    *          no entry contains the specified key.
    */
 
-  public Entry remove(Object key) {
-    // Replace the following line with your solution.
-    int correctIndex = compFunction(key.hashCode());
+  public Entry remove(Object key) {    int correctIndex = compFunction(key.hashCode());
     DList correctDList = this.table[correctIndex];
     if (correctDList == null) {
         return null;
@@ -227,7 +180,6 @@ public class HashTableChained implements Dictionary {
             return toReturn;
             }
         catch (InvalidNodeException e3) {
-            //System.out.println("INVALID NODE");
             return null;
         }
     }
@@ -240,9 +192,7 @@ public class HashTableChained implements Dictionary {
    *  Remove all entries from the dictionary.
    */
   public void makeEmpty() {
-    // Your solution here.
       for (int i = 0; i < table.length; i++) {
-          //System.out.println( "hello " + i);
           DList insertion = new DList();
           this.table[i] = insertion;
       }
@@ -262,27 +212,4 @@ public class HashTableChained implements Dictionary {
       return toReturn;
   }
   
-  public static void main(String[] argv) {
-	  	HashTableChained table = new HashTableChained();
-	  	table.insert("test1", "hi");
-	  	table.insert("wolf", "howl");
-	  	table.insert("fox", "idkbro");
-	  	table.insert("test2", "bye");
-	  	table.insert(12, 10);
-	  	Entry a = table.find("test1");
-	  	Entry b = table.find("wolf");
-	  	table.remove("fox");
-	  	Entry c = table.find("fox");
-                System.out.println(table);
-                System.out.println(a);
-                System.out.println(b);
-                System.out.println(c);
-                table.insert("test", "test1");
-                System.out.println(table);
-                table.remove("test");
-                System.out.println(table);
-	  	System.out.println("pls be size 4: "+table.size());
-  }
-
-
 }
