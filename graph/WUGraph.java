3817b79a36632bd74d63d31ccbeb8f234968deb1
Luke Levis <luke.levis@berkeley.edu>
2014-04-19 00:15:57 -0700

    fix

--- graph/WUGraph.java
+++ graph/WUGraph.java
@@ -57,7 +57,7 @@ public class WUGraph {
    * Running time:  O(|V|).
    */
   public Object[] getVertices() {
-    Object[] toReturn = new Object[vertices.length()];
+    Object[] toReturn = new Object[vCount];
     int index = 0;
     DListNode curr = (DListNode)vertices.front();
     try {
