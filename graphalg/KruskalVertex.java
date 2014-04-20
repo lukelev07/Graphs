23171d9edefcb0f51560446469971f9561fe7b0d
Gurjit Singh <gurjit.singh@berkeley.edu>
2014-04-19 22:30:51 -0700

    added comments

--- graphalg/KruskalVertex.java
+++ graphalg/KruskalVertex.java
@@ -12,11 +12,18 @@ public class KruskalVertex {
         myObj = vertex;
         this.value = value;
     }
-
+    /*
+     * method to return the int value associated with this object
+     * @return int value
+     */
     public int getValue(){
         return value;
     }
 
+    /*
+     * method to return the object associated in this Vertex
+     * @return Object myObj
+     */
     public Object getObject(){
         return myObj;
     }
